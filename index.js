6321eea3a68ae1da588f99cef64ef9c8235f4be1
NAVEED SALEEM <naveedsaleem2017@gmail.com>
2019-11-01 21:40:42 -0400

    javascript II

--- index.js
+++ index.js
@@ -48,8 +48,11 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
-  /* CODE HERE */
+function processLength(list, callback) {
+
+  const length = list.length;
+  return callback(length);
+  
 }
 
 /**
@@ -66,8 +69,8 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
-  /* CODE HERE */
+function processLastItem(stringList, callback) {
+  return callback(stringList[stringList.length-1]);
 }
 
 /**
@@ -87,9 +90,13 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function processSum(numberlist, callback) {
+  let tot = numberlist.reduce((total, num) => {
+    return total += num;
+  }, 0);
+    return callback(tot)
+
+ }
 
 /**
  * ### Challenge `processProduct`
@@ -109,8 +116,8 @@ function processSum(/* CODE HERE */) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
 */
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+function processProduct(num1, num2, callback) {
+  return callback(num1 * num2);
 }
 
 /**
